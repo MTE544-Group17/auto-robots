0bf501cd44300d6472970007057b9cb070629158
Rishab Sareen <rishabhsareen@gmail.com>
2017-10-25 16:50:34 -0400

    Adding code from Lab 1, located inside catkin_ws

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/rishab/catkin_ws/src
+# Build directory: /home/rishab/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
+subdirs(turtlebot_example)
