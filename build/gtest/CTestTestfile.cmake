57d9b6fe45d8bba3259fd1936b505367d3384591
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-10-25 17:16:02 -0400

    Clear repo

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/mihai/Documents/Github/auto-robots/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- lab1/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rishab/catkin_ws/src
-# Build directory: /home/rishab/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(beginner_tutorials)
-subdirs(turtlebot_example)
