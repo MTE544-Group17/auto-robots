996ec5e70366e4a1a8f80bf2117f2519e62a00df
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-11-01 15:40:22 -0400

    Working particle filter

--- lab2_package/src/turtlebot_example_node_lab2.cpp
+++ lab2_package/src/turtlebot_example_node_lab2.cpp
@@ -35,7 +35,7 @@ double ips_x;
 double ips_y;
 double ips_yaw;
 
-int num_particles = 300;
+int num_particles = 600;
 
 //Create a stuct to define the cornidates and weight of the particle
 struct Particle {
@@ -72,21 +72,16 @@ double dang = 0;
 auto t1 = chrono::high_resolution_clock::now();
 auto t2 = chrono::high_resolution_clock::now();
 
-bool check_odo = true;
 
 void odometry_callback(const nav_msgs::Odometry& msg) {
-  if(check_odo) {
-    t1 = t2;
-    t2 = chrono::high_resolution_clock::now();
+  t1 = t2;
+  t2 = chrono::high_resolution_clock::now();
 
-    double t_delta = ((double) std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()) / (1000000000.0);
+  double t_delta = ((double) std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()) / (1000000000.0);
 
-    dx = t_delta * msg.twist.twist.linear.x;
-    dy = t_delta * msg.twist.twist.linear.y;
-    dang = t_delta * msg.twist.twist.angular.z;
-  }
-
-  check_odo = !check_odo;
+  dx = t_delta * msg.twist.twist.linear.x;
+  dy = t_delta * msg.twist.twist.linear.y;
+  dang = t_delta * msg.twist.twist.angular.z;
 }
 
 //Callback function for the Position topic (LIVE)
@@ -165,9 +160,10 @@ void random_particle_allocator() {
   }
 }
 
-<<<<<<< HEAD
 void move_particles() {
+  default_random_engine generator;
   for(auto &particle:particle_set) {
+    // ROS_INFO("MOVING");
     // particle.yaw += dang * (1.0 + random_gen() * 0.08 - 0.04);
     // double dist = sqrt(dx*dx + dy*dy) * (1.0 + random_gen() * 0.08 - 0.04);
     // particle.x += dist * cos(particle.yaw);
@@ -183,8 +179,14 @@ void move_particles() {
     // particle.x += dist * cos(particle.yaw);
     // particle.y += dist * sin(particle.yaw);
 
-    particle.yaw += dang + (random_gen() * 0.08 - 0.04);
-    double dist = sqrt(dx*dx + dy*dy) + (random_gen() * 0.3 - 0.15);
+    normal_distribution<double> distro_ang(0.0, dang * 0.2);
+    particle.yaw += dang + distro_ang(generator);
+
+    double dist = sqrt(dx*dx + dy*dy);
+    // normal_distribution<double> distro_dist(0.0, dist * 0.5);
+    normal_distribution<double> distro_dist(0.0, dist * 0.8);
+    dist += distro_dist(generator);
+    // double dist = sqrt(dx*dx + dy*dy) * ( 1.0 + random_gen() * 0.2 - 0.1);
     particle.x += dist * cos(particle.yaw);
     particle.y += dist * sin(particle.yaw);
 
@@ -194,37 +196,36 @@ void move_particles() {
     // particle.y += dist * sin(particle.yaw);
   }
 }
-=======
-particle create_particle () {
-  particle p;
-  p.x = (double)(rand() * 1000000) / 1000000.0 * MAP_SIZE;
-  p.y = (double)(rand() * 1000000) / 1000000.0 * MAP_SIZE;
->>>>>>> 1e8db057240960ff373bf573c6f98414bdb13c46
 
 double distanceToIPS(Particle p) {
-  double dist = sqrt((p.x - ips_x) * (p.x - ips_x) + (p.y - ips_y) * (p.y - ips_y));
-  return dist;
+  return abs(sqrt((p.x - ips_x) * (p.x - ips_x) + (p.y - ips_y) * (p.y - ips_y)));
 }
 
 double angDiff(Particle p) {
-  double ang = abs(p.yaw - ips_yaw);
-  if(ang >= M_PI) {
-    ang = 2 * M_PI - ang;
+  double ang = p.yaw - ips_yaw;
+
+  if(abs(ang) >= M_PI) {
+    return abs(ang) - M_PI;
+  } else {
+    return abs(ang);
   }
-  return ang;
 }
 
 double max_weight = 0.0;
 double total_weight = 0.0;
 
 void particle_weighting() {
+  // ROS_INFO("WEIGHING");
+
   max_weight = 0.0;
+  total_weight = 0.0;
   for(auto &particle:particle_set) {
-    double denom = (distanceToIPS(particle) + angDiff(particle));
+    // double denom = distanceToIPS(particle) + angDiff(particle) * 0.2;
+    double denom = distanceToIPS(particle) + angDiff(particle);
     // double denom = (distanceToIPS(particle));
-    if(denom < 0.0001) {
-      denom  = 0.0001;
-    }
+    // if(denom < 0.0001) {
+    //   denom  = 0.0001;
+    // }
     particle.weight = 1.0 / denom;
 
     // ROS_INFO("Inner:  %f", particle.weight);
@@ -240,19 +241,23 @@ void particle_weighting() {
 }
 
 void resampling() {
+  // ROS_INFO("RESAMPLING,      max_weight : %f", max_weight);
   vector<Particle> newParticle_set;
-  int index = rand() % (num_particles - 1);
+  int index = rand() % num_particles;
   double beta = 0.0;
   for(int i = 0; i < num_particles; i++){
-    beta += random_gen() * 2.0 * max_weight;
+    // beta += random_gen() * 2.0 * max_weight;
+    beta += random_gen() * 2.0;
     while(beta > particle_set[index].weight){
+      // ROS_INFO("RESAMPLING LOOP,      beta: %f,         index: %d", beta, index);
       beta -= particle_set[index].weight;
-      index = (index + 1) % (num_particles - 1);
+      index = (index + 1) % num_particles;
     }
     newParticle_set.push_back(particle_set[index]);
   }
 
   particle_set = newParticle_set;
+  // ROS_INFO("RESAMPLING DONE");
 }
 
 double predicted_x = 0;
@@ -260,6 +265,7 @@ double predicted_y = 0;
 double predicted_yaw = 0;
 
 void predict_pos() {
+  // ROS_INFO("MOVING");
   predicted_x = 0;
   predicted_y = 0;
 
@@ -300,19 +306,47 @@ int main(int argc, char **argv)
 
     random_particle_allocator();
 
-    visualization_msgs::Marker points;
-    points.header.frame_id = "/my_frame";
-    points.header.stamp = ros::Time::now();
-    points.ns = "points";
-    points.action = visualization_msgs::Marker::ADD;
-    points.type = visualization_msgs::Marker::POINTS;
-    points.id = 1;
+    // visualization_msgs::Marker points;
+    // points.header.frame_id = "/my_frame";
+    // points.header.stamp = ros::Time::now();
+    // points.ns = "points";
+    // points.action = visualization_msgs::Marker::ADD;
+    // points.type = visualization_msgs::Marker::POINTS;
+    // points.id = 1;
+    //
+    // points.scale.x = 0.15;
+    // points.scale.y = 0.15;
+    //
+    // points.color.r = 1.0f;
+    // points.color.a = 1.0;
 
-    points.scale.x = 0.15;
-    points.scale.y = 0.15;
+    visualization_msgs::Marker line_strip;
+    line_strip.header.frame_id = "/my_frame";
+    line_strip.header.stamp = ros::Time::now();
+    line_strip.ns = "line_strip";
+    line_strip.action = visualization_msgs::Marker::ADD;
+    line_strip.type = visualization_msgs::Marker::LINE_STRIP;
+    line_strip.id = 1;
 
-    points.color.r = 1.0f;
-    points.color.a = 1.0;
+    line_strip.scale.x = 0.15;
+    line_strip.scale.y = 0.15;
+
+    line_strip.color.g = 1.0f;
+    line_strip.color.a = 1.0;
+
+    visualization_msgs::Marker base_line;
+    base_line.header.frame_id = "/my_frame";
+    base_line.header.stamp = ros::Time::now();
+    base_line.ns = "base_line";
+    base_line.action = visualization_msgs::Marker::ADD;
+    base_line.type = visualization_msgs::Marker::LINE_STRIP;
+    base_line.id = 1;
+
+    base_line.scale.x = 0.15;
+    base_line.scale.y = 0.15;
+
+    base_line.color.b = 1.0f;
+    base_line.color.a = 0.5;
 
     visualization_msgs::Marker base_point;
     base_point.header.frame_id = "/my_frame";
@@ -322,42 +356,35 @@ int main(int argc, char **argv)
     base_point.type = visualization_msgs::Marker::POINTS;
     base_point.id = 2;
 
-    base_point.scale.x = 0.1;
-    base_point.scale.y = 0.1;
+    base_point.scale.x = 0.3;
+    base_point.scale.y = 0.3;
 
-    base_point.color.r = 1.0f;
-    base_point.color.g = 1.0f;
     base_point.color.b = 1.0f;
     base_point.color.a = 1.0;
 
-    bool update = true;
-
     while (ros::ok())
     {
+      // ROS_INFO("LOOP START");
     	loop_rate.sleep(); //Maintain the loop rate
     	ros::spinOnce();   //Check for new messages
 
     	//Main loop code goes here:
-    	vel.linear.x = 0.5; // set linear speed
-    	vel.angular.z = 0.5; // set angular speed
+    	// vel.linear.x = 1.0; // set linear speed
+    	// vel.angular.z = 1.0; // set angular speed
 
-      if(update) {
-        move_particles();
-        particle_weighting();
-        resampling();
-      }
+      move_particles();
+      particle_weighting();
+      resampling();
 
-      update = !update;
-
-      vector<geometry_msgs::Point> points_vec;
-      for(auto &particle:particle_set) {
-        geometry_msgs::Point p;
-        p.x = particle.x;
-        p.y = particle.y;
-        p.z = 0;
-        points_vec.push_back(p);
-      }
-      points.points = points_vec;
+      // vector<geometry_msgs::Point> points_vec;
+      // for(auto &particle:particle_set) {
+      //   geometry_msgs::Point p;
+      //   p.x = particle.x;
+      //   p.y = particle.y;
+      //   p.z = 0;
+      //   points_vec.push_back(p);
+      // }
+      // points.points = points_vec;
 
       // predict_pos();
       //
@@ -370,21 +397,34 @@ int main(int argc, char **argv)
       // points.points = points_vec;
 
 
+      predict_pos();
+
+      geometry_msgs::Point p;
+      p.x = predicted_x;
+      p.y = predicted_y;
+      p.z = 0;
+      line_strip.points.push_back(p);
+
+
       geometry_msgs::Point base;
       base.x = ips_x;
       base.y = ips_y;
       base.z = 0;
       vector<geometry_msgs::Point> base_vec;
+      base_line.points.push_back(base);
       base_vec.push_back(base);
       base_point.points = base_vec;
 
-      marker_pub.publish(points);
+      // marker_pub.publish(points);
+      marker_pub.publish(line_strip);
+      marker_pub.publish(base_line);
       marker_pub.publish(base_point);
 
       // ROS_INFO("IPS: (%f, %f, %f)", ips_x, ips_y, ips_yaw);
       // ROS_INFO("Predicted: (%f, %f, %f)", predicted_x, predicted_y, predicted_yaw);
 
     	velocity_publisher.publish(vel); // Publish the command velocity
+      // ROS_INFO("LOOP END");
     }
 
     return 0;
