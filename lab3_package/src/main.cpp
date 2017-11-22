cbdb228f6f26c80944493cb6c82bc9d4655b4773
Mihai Tiuca <teodor.tiuca@gmail.com>
2017-11-22 18:03:06 -0500

    Generating points and nearest neighbours

--- lab3_package/src/main.cpp
+++ lab3_package/src/main.cpp
@@ -18,12 +18,33 @@
 #include <nav_msgs/OccupancyGrid.h>
 #include <vector>
 // #include <iostream>
+// #include <string>
 // #include <ctime>
 // #include <chrono>
 
 #include <cmath>
 
+const int MAP_SIZE = 100;
+const int ROBOT_WIDTH = 3;
+const int NUM_POINTS = 400;
+const int NUM_CONNECTIONS = 8;
+
 using namespace std;
+
+int collisionMap[MAP_SIZE * MAP_SIZE];
+
+bool mapReady = false;
+
+struct Point {
+  int x = 0;
+  int y = 0;
+  vector<int> nearestNeighbours;
+};
+
+struct Neighbour {
+  int pointIndex;
+  double dist;
+};
 
 ros::Publisher pose_publisher;
 ros::Publisher marker_pub;
@@ -61,7 +82,15 @@ void pose_callback(const geometry_msgs::PoseWithCovarianceStamped& msg)
 
 //Callback function for the map
 void map_callback(const nav_msgs::OccupancyGrid& msg) {
+  ROS_INFO("Map Width : %d", msg.info.width);
+  ROS_INFO("Map Height : %d", msg.info.height);
 
+  for(int i = 0; i < MAP_SIZE * MAP_SIZE; i++) {
+    collisionMap[i] = (int) msg.data[i];
+  }
+
+  ROS_INFO("Map Ready ...");
+  mapReady = true;
 }
 
 //Bresenham line algorithm (pass empty vectors)
@@ -104,6 +133,91 @@ void bresenham(int x0, int y0, int x1, int y1, vector<int>& x, vector<int>& y) {
     }
 }
 
+vector<Point> pointMap;
+
+void generateRandomMap() {
+  for(int i = 0; i < NUM_POINTS; i++) {
+    Point newPoint;
+    newPoint.x = (int) (random_gen() * (MAP_SIZE - 2) + 1);
+    newPoint.y = (int) (random_gen() * (MAP_SIZE - 2) + 1);
+
+    pointMap.push_back(newPoint);
+  }
+}
+
+bool inCollision(int index) {
+  int min_x = index % 100 - ROBOT_WIDTH;
+  int max_x = index % 100 + ROBOT_WIDTH;
+
+  int min_y = (int)(index / 100) - ROBOT_WIDTH;
+  int max_y = (int)(index / 100) + ROBOT_WIDTH;
+
+  if(min_x < 0 || max_x > MAP_SIZE) { return true; }
+  if(min_y < 0 || max_y > MAP_SIZE) { return true; }
+
+  for(int x = min_x; x < max_x; x++) {
+    for(int y = min_y; y < max_y; y++) {
+      if(collisionMap[y*100 + x] > 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+void removeCollisionPoints() {
+  ROS_INFO("Number of points : %d", (int) pointMap.size());
+  ROS_INFO("Removing points in collision....");
+
+  vector<Point>::iterator p = pointMap.begin();
+
+  while(p != pointMap.end()) {
+    int index = p->y * 100 + p->x;
+
+    if(inCollision(index)) {
+      p = pointMap.erase(p);
+    } else {
+      p++;
+    }
+  }
+
+  ROS_INFO("Final number of points : %d\n", (int) pointMap.size());
+}
+
+bool compareByDistance(const Neighbour &a, const Neighbour &b) {
+    return a.dist < b.dist;
+}
+
+void connectNearestNeighbours() {
+  for(int pIndex = 0; pIndex < pointMap.size(); pIndex++) {
+    vector<Neighbour> allNeighbours;
+
+    for(int nIndex = 0; nIndex < pointMap.size(); nIndex++) {
+      if(nIndex == pIndex) { continue; }
+
+      Neighbour n;
+      n.pointIndex = nIndex;
+      n.dist = sqrt(pow(pointMap[nIndex].x - pointMap[pIndex].x, 2) + pow(pointMap[nIndex].y - pointMap[pIndex].y, 2));
+      allNeighbours.push_back(n);
+    }
+
+    sort(allNeighbours.begin(), allNeighbours.end(), compareByDistance);
+
+    for(int i = 0; i < NUM_CONNECTIONS; i++) {
+      pointMap[pIndex].nearestNeighbours.push_back(allNeighbours[i].pointIndex);
+    }
+  }
+
+  for(int pIndex = 0; pIndex < pointMap.size(); pIndex++) {
+    ROS_INFO("Point %d : [%d %d %d %d %d]", pIndex, pointMap[pIndex].nearestNeighbours[0], pointMap[pIndex].nearestNeighbours[1], pointMap[pIndex].nearestNeighbours[2], pointMap[pIndex].nearestNeighbours[3], pointMap[pIndex].nearestNeighbours[4]);
+  }
+}
+
+void removeCollisionPaths() {
+  
+}
+
 int main(int argc, char **argv)
 {
 	  //Initialize the ROS framework
@@ -125,14 +239,74 @@ int main(int argc, char **argv)
     //Set the loop rate
     ros::Rate loop_rate(20);    //20Hz update rate
 
+    visualization_msgs::Marker points;
+    points.header.frame_id = "/my_frame";
+    points.header.stamp = ros::Time::now();
+    points.ns = "points";
+    points.action = visualization_msgs::Marker::ADD;
+    points.type = visualization_msgs::Marker::POINTS;
+    points.id = 1;
+
+    points.scale.x = 0.8f;
+    points.scale.y = 0.8f;
+
+    points.color.r = 1.0f;
+    points.color.a = 1.0f;
+
+    visualization_msgs::Marker collisionPoints;
+    collisionPoints.header.frame_id = "/my_frame";
+    collisionPoints.header.stamp = ros::Time::now();
+    collisionPoints.ns = "collisionPoints";
+    collisionPoints.action = visualization_msgs::Marker::ADD;
+    collisionPoints.type = visualization_msgs::Marker::POINTS;
+    collisionPoints.id = 1;
+
+    collisionPoints.scale.x = 0.8f;
+    collisionPoints.scale.y = 0.8f;
+
+    collisionPoints.color.r = 0.8f;
+    collisionPoints.color.g = 0.8f;
+    collisionPoints.color.b = 1.0f;
+    collisionPoints.color.a = 1.0f;
+
     while (ros::ok())
     {
     	loop_rate.sleep(); //Maintain the loop rate
     	ros::spinOnce();   //Check for new messages
 
     	//Main loop code goes here:
-    	vel.linear.x = 1.0; // set linear speed
-    	vel.angular.z = 1.0; // set angular speed
+    	// vel.linear.x = 1.0; // set linear speed
+    	// vel.angular.z = 1.0; // set angular speed
+
+      if(mapReady) {
+        generateRandomMap();
+        removeCollisionPoints();
+        connectNearestNeighbours();
+        mapReady = false;
+
+        vector<geometry_msgs::Point> displayPointsVec;
+        for(auto &point:pointMap) {
+          geometry_msgs::Point p;
+          p.x = point.x - 50;
+          p.y = point.y - 50;
+          displayPointsVec.push_back(p);
+        }
+        points.points = displayPointsVec;
+
+        vector<geometry_msgs::Point> displayCollisionPointsVec;
+        for(int i =0; i < MAP_SIZE * MAP_SIZE; i++) {
+          if(collisionMap[i] > 0) {
+            geometry_msgs::Point p;
+            p.x = i%100 - 50;
+            p.y = (int) (i /100) - 50;
+            displayCollisionPointsVec.push_back(p);
+          }
+        }
+        collisionPoints.points = displayCollisionPointsVec;
+      }
+
+      marker_pub.publish(points);
+      marker_pub.publish(collisionPoints);
 
     	velocity_publisher.publish(vel); // Publish the command velocity
     }
